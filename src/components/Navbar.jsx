0824efb57692e98fed2306d8983f269cbfb7d51d
hoangng26 <hoangnguyenhk14cqt@gmail.com>
2023-06-16 04:26:14 +0700

    Fix bugs: Handle cancel event when upload and modify logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { UserOutlined } from '@ant-design/icons';
+import { AntCloudOutlined, UserOutlined } from '@ant-design/icons';
 import { Avatar, Input } from 'antd';
 import Link from 'next/link';
 
@@ -6,7 +6,12 @@ export default function Navbar() {
   return (
     <>
       <Link href="/">
-        <h1 className="font-bold text-2xl text-slate-50">Distributed Storage</h1>
+        <h1 className="font-bold text-2xl text-slate-50">
+          <span>
+            <AntCloudOutlined />
+          </span>{' '}
+          Distributed Storage
+        </h1>
       </Link>
       <Input.Search className="w-96 flex justify-center" placeholder="Search" size="large" />
       <Avatar size="large" icon={<UserOutlined />} />
--- src/components/UploadFile.jsx
+++ src/components/UploadFile.jsx
@@ -70,6 +70,9 @@ export default function UploadFile({ activeServers, onUpdateListFiles }) {
         }}
         onOk={handleUploadEvent}
         onCancel={() => {
+          if (buttonLoading) {
+            return;
+          }
           formRef.current.reset();
           setShowUpload(false);
         }}

7c71773427bd529ca320c0cf5a95b30535ed04d1
hoangng26 <hoangnguyenhk14cqt@gmail.com>
2023-06-20 17:45:05 +0700

    Changes: Update Active Status on UI

--- src/components/FileActions.jsx
+++ src/components/FileActions.jsx
@@ -1,4 +1,4 @@
-import { CloseCircleOutlined } from '@ant-design/icons';
+import { CheckCircleTwoTone, CloseCircleOutlined, CloseSquareTwoTone } from '@ant-design/icons';
 import { Button } from 'antd';
 import DeleteFiles from './DeleteFiles';
 import DownloadFiles from './DownloadFiles';
@@ -15,7 +15,19 @@ export default function FileActions({
   return (
     <>
       <div className="mt-8">
-        <div className="font-bold pb-4 text-xl">{selectedServer ? `IP Address: ${selectedServer}` : 'All Servers'}</div>
+        <div className="font-bold text-xl">{selectedServer ? `IP Address: ${selectedServer}` : 'All Servers'}</div>
+        <div className="font-bold pb-4 text-xl">
+          <span>
+            Active:{' '}
+            {selectedServer &&
+              (activeServers.includes(selectedServer) ? (
+                <CheckCircleTwoTone twoToneColor="#52c41a" />
+              ) : (
+                <CloseSquareTwoTone twoToneColor="red" />
+              ))}
+            {!selectedServer && <CheckCircleTwoTone twoToneColor="#52c41a" />}{' '}
+          </span>
+        </div>
       </div>
 
       {listFiles.length > 0 && (
